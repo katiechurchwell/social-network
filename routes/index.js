4d3ae3d360b11394226e01e6b29b8ae960120446
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-11 11:44:28 -0800

    user routes

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+// no prefix
+router.use('/', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('404 Not Found');
+});
+
+module.exports = router;
